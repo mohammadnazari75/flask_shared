7ca0db8d719290ceb3784d248969ed6b4c242488
Mohammad Nazari <nazari@cafebazaar.ir>
2020-12-23 00:05:42 +0330

    make a uuid app

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+   name="shared",
+   version="1.0.0",
+   description="Our microservices shared library",
+   author="mohammad nazari",
+   author_email="mohammadnazari75@gmail.com",
+   packages=find_packages()
+)
---
+++ shared/__init__.py
@@ -0,0 +1,1 @@
+from shared.uuid import uuidgen
